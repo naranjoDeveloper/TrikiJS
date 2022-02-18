c9e0d79b5877a447e031084c5e014e4d8f075e66
Juan Manuel Doria <juanmadoria98@gmail.com>
2022-02-18 09:55:08 -0500

    Codigo optimizado

--- triqui.js
+++ triqui.js
@@ -7,77 +7,23 @@ function resetJuego() {
 
 
 function pintarX(lugar) {
-    let cuadro = posicion[lugar];
-    cuadro.classList.add('equis');
-    console.log(cuadro);
+    posicion[lugar].classList.add('equis');
     player++;
 }
 
 function pintarO(lugar) {
-    let cuadro = posicion[lugar];
-    cuadro.classList.add('circulo');
-    console.log(cuadro);
+    posicion[lugar].classList.add('circulo');
     player++;
 }
 
 posicion.forEach(cuadro => {
     cuadro.addEventListener('mousedown', () => {
         if (player % 2 == 0) {
-            if (cuadro.id == 1) {
-                pintarX(0);
-            }
-            if (cuadro.id == 2) {
-                pintarX(1);
-            }
-            if (cuadro.id == 3) {
-                pintarX(2);
-            }
-            if (cuadro.id == 4) {
-                pintarX(3);
-            }
-            if (cuadro.id == 5) {
-                pintarX(4);
-            }
-            if (cuadro.id == 6) {
-                pintarX(5);
-            }
-            if (cuadro.id == 7) {
-                pintarX(6);
-            }
-            if (cuadro.id == 8) {
-                pintarX(7);
-            }
-            if (cuadro.id == 9) {
-                pintarX(8);
-            }
+            let enX = cuadro.id - 1;
+            pintarX(enX)
         } else {
-            if (cuadro.id == 1) {
-                pintarO(0);
-            }
-            if (cuadro.id == 2) {
-                pintarO(1);
-            }
-            if (cuadro.id == 3) {
-                pintarO(2);
-            }
-            if (cuadro.id == 4) {
-                pintarO(3);
-            }
-            if (cuadro.id == 5) {
-                pintarO(4);
-            }
-            if (cuadro.id == 6) {
-                pintarO(5);
-            }
-            if (cuadro.id == 7) {
-                pintarO(6);
-            }
-            if (cuadro.id == 8) {
-                pintarO(7);
-            }
-            if (cuadro.id == 9) {
-                pintarO(8);
-            }
+            let enY = cuadro.id - 1
+            pintarO(enY)
         }
 
     });
