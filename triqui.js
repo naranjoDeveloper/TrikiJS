c376486c0120ec6f9cba0a7e446850db3b796cd4
naranjo <naranjo.developer@gmail.com>
2022-02-18 17:36:23 -0500

    COMPROBACION TRIQUI CON NUMEROS MAGICOOOS

--- triqui.js
+++ triqui.js
@@ -2,6 +2,20 @@ let posicion = document.querySelectorAll('.cuadro');
 let player = 0;
 
 function resetJuego() {
+    let c = document.querySelectorAll('.circulo');
+    let x = document.querySelectorAll('.equis');
+
+    c.forEach(circulo=>{
+        circulo.classList.remove('circulo')
+        
+    })
+
+    x.forEach(equis=>{
+        equis.classList.remove('equis');
+    })
+
+    player = 0;
+
 
 }
 
@@ -30,25 +44,112 @@ posicion.forEach(cuadro => {
         if (player % 2 == 0) {
             let enX = cuadro.id - 1;
             pintarX(enX)
+            checkT()
         } else {
             let enY = cuadro.id - 1
             pintarO(enY)
+            checkT()
         }
-        triqui();
-
     });
 });
 
-//
-function triqui() {
-    //ToDo
-    console.log('Estas en el triqui');
-}
 
 function lugarOcupado(lugar) {
 
     if (posicion[lugar].classList.contains('circulo') || posicion[lugar].classList.contains('equis')) {
         return true;
     }
+
+}
+
+function checkT() {
+
+    let c = document.querySelectorAll('.circulo');
+    let x = document.querySelectorAll('.equis');
+
+    let aC = [];
+    let aX = [];
+
+    x.forEach(equis => {
+        aX.push(parseInt(equis.id))
+
+    })
+
+    c.forEach(circulo => {
+        aC.push(parseInt(circulo.id))
+    })
+
+    console.log(aX)
+    console.log(aC)
+
+    if(aX.includes(1) && aX.includes(2) && aX.includes(3)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aX.includes(4) && aX.includes(5) && aX.includes(6)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aX.includes(7) && aX.includes(8) && aX.includes(9)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aX.includes(1) && aX.includes(4) && aX.includes(7)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aX.includes(2) && aX.includes(5) && aX.includes(8)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aX.includes(3) && aX.includes(6) && aX.includes(9)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aX.includes(1) && aX.includes(5) && aX.includes(9)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aX.includes(3) && aX.includes(5) && aX.includes(7)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }
+
+    if(aC.includes(1) && aC.includes(2) && aC.includes(3)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aC.includes(4) && aC.includes(5) && aC.includes(6)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aC.includes(7) && aC.includes(8) && aC.includes(9)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aC.includes(1) && aC.includes(4) && aC.includes(7)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aC.includes(2) && aC.includes(5) && aC.includes(8)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aC.includes(3) && aC.includes(6) && C.includes(9)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aC.includes(1) && aC.includes(5) && aC.includes(9)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }else if(aC.includes(3) && aC.includes(5) && aC.includes(7)){
+        setTimeout(() => {
+            alert('gano')
+        }, 500);
+    }
+
+
 
 }
